f885876a68154fc791852d4d039f063a5090fc87
Attila Álmos Szabó <ifjmacila@gmail.com>
2016-11-18 14:35:46 +0100

    related really fixed

--- ConsoleApplication2/Program.fs
+++ ConsoleApplication2/Program.fs
@@ -5,21 +5,20 @@ type Cell =
     | Fixed of int
     | Open of Set<int>
 
-type Board = Cell[,]
+type Board = Map<int * int, Cell>
 
 let related (x, y) =
-  
     seq {
         for i  in 0..8 ->(i,y)
         for i  in 0..8 ->(x,i)
-        for i  in x/3..x/3+2 do
-            for j in y/3..y/3+2 
-             ->(x,y)} |> Set.ofSeq |> Set.remove (x,y)
+        for i  in x/3 .. x/3 + 2 do
+            for j in y/3 .. y/3 + 2 
+             ->(i,j)} |> Set.ofSeq |> Set.remove (x,y)
    
    
 let lines = System.IO.File.ReadAllLines("C:/Users/valaki/Desktop/input.txt")
 
-let board = Array2D.init 9 9 (fun i j ->
+let input = Array2D.init 9 9 (fun i j ->
                      match (int)lines.[i].[j .. j] with
                      | x when x>0 -> Some x 
                      | _ -> None ) 
@@ -29,7 +28,8 @@ let get (x, y) (arr: _[,]) = arr.[x, y]
 let pos = 3, 4
 
 let e = board |> get pos
-
+related(2,2) |> List.ofSeq
+ |> List.length
 
 
 
