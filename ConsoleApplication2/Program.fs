26cc6b331d870a0fe58f83e14e0f5e3caedd662a
valaki <valaki@PC22603>
2016-12-02 15:49:18 +0100

    solve function OK

--- ConsoleApplication2/Program.fs
+++ ConsoleApplication2/Program.fs
@@ -4,14 +4,26 @@
 type Cell =
     | Fixed of int
     | Open of Set<int>
+    override self.ToString() =
+        match self with
+        | Fixed f -> sprintf "%i" f
+        | Open s when s.IsEmpty -> sprintf "x"
+        | Open _ -> sprintf("?") 
+        
 
 type Board = Map<int * int, Cell>
-
+   
+let printBoard (board:Board) =
+    for i = 0 to 8 do
+        for j = 0 to 8 do
+            printf "%O" board.[i,j]
+        printfn ""
+        
 type Guess =
     | NextBoard of Board 
     | NextGuess of ((int*int)*Set<int>)
     | Invalid
-    | Complete
+    | Complete of Board
 let related (x, y) =
     let ib = x/3 * 3
     let jb= y/3 * 3
@@ -32,32 +44,30 @@ System.Environment.CurrentDirectory
 
 let lines =File.ReadAllLines(Path.Combine (__SOURCE_DIRECTORY__,"input.txt"))
 
+
+            
 let input = 
     Array2D.init 9 9 (fun i j ->
         match (int)lines.[i].[j .. j] with
         | x when x>0 -> Some x 
         | _ -> None ) 
 let input2= 
- seq {
+    seq {
         for i  in 0..8  do  
             for j in 0..8 do
                if (int)lines.[i].[j .. j]>0  then yield((i,j),(int)lines.[i].[j .. j])
     } |>Seq.toList
 
-let get (x, y) (arr: _[,]) = arr.[x, y]
-
-let pos = 3, 4
-
-let numbers = Set.ofSeq [1..9]
+let numbers = Set { 1 .. 9 }
 
 let emptyCell = 
     Open(numbers)
 
 let emptyBoard =
-    seq {
+    Map [
         for i  in 0..8  do
             for j  in 0..8 ->((i,j),emptyCell)
-        } |>Map.ofSeq
+    ]
  
 
 
@@ -75,21 +85,48 @@ let nextGuess (board:Board) :Guess =
           match cell with
            |(pos,Open s)-> Some (pos,(s, Set.count s))
            |_-> None) |>Seq.toList
-    if List.isEmpty opens then Complete else
+    if List.isEmpty opens then Complete(board) else
     let pos,(set, count) = opens |> List.minBy (snd >> snd)
     match count with
-    |0 ->  Invalid
+    |0 -> Invalid
     |1 -> fixCell board (pos,Set.minElement set) |> NextBoard
     |_ -> (pos,set) |> NextGuess
     
 
 let nextBoard = nextGuess startingBoard
 
-let e = board |> get pos
-related(2,2) |> List.ofSeq
- |> List.length
+let rec solve (board:Board) = 
+    let rec solveGuess (guess:Guess)=
+        match guess with
+        | NextBoard g -> nextGuess g |> solveGuess
+        | NextGuess (pos,set) ->
+            let s =
+                set |> Seq.tryPick (fun v ->
+                   fixCell board (pos,v) |> solve
+                )
+            match s with
+            | Some g -> Complete(g)
+            | None -> Invalid
+        | Invalid -> Invalid 
+        | Complete g -> Complete(g)
+    match solveGuess (nextGuess board) with
+    |Complete g -> Some g
+    |_ -> None
+
+        
+solve startingBoard |>Option.iter printBoard     
+    
+solve emptyBoard |>Option.iter printBoard     
+    
 
 
+//let e = board |> get pos
+//related(2,2) |> List.ofSeq
+// |> List.length
+
+let arr2 = Array2D.zeroCreate<int> 9 9
+
+arr2.[0 .. 2, 0 .. 2]
 
 [<EntryPoint>]
 let main argv = 
